cf34a63286d00b809c0cbf68db4577aa7aeefbd0
Valery Basova <basovavalery@gmail.com>
2022-08-18 15:40:18 -0400

    add unit test for livedata in SaveReminderViewModel

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -3,11 +3,16 @@ package com.udacity.project4.locationreminders.savereminder
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
+import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -60,8 +65,11 @@ class SaveReminderViewModelTest {
         // When validate title is null or Empty
         val isTitleValid = saveReminderViewModel.validateEnteredData(reminderDataItem1)
 
-        // Then there is false returned
+        // Then there is false returned and snackBar is set to error message
         assertThat(isTitleValid, `is`(false))
+
+        val value = saveReminderViewModel.showSnackBarInt.getOrAwaitValue()
+        assertThat(value, `is`(R.string.err_enter_title))
     }
 
     @Test
@@ -71,8 +79,47 @@ class SaveReminderViewModelTest {
         // When validate title is null or Empty
         val isLocatonValid = saveReminderViewModel.validateEnteredData(reminderDataItem2)
 
-        // Then there is false returned
+        // Then there is false returned and snackBar is set to error message
         assertThat(isLocatonValid, `is`(false))
+
+        val value = saveReminderViewModel.showSnackBarInt.getOrAwaitValue()
+        assertThat(value, `is`(R.string.err_select_location))
     }
 
+    @Test
+    fun onClear() {
+        // Given a Point of Interest to save in the viewModel livedata
+        val poi =  PointOfInterest(
+            LatLng(37.4222359923114, -122.08400387119623),
+            "0002",
+            "googleplex"
+        )
+        saveReminderViewModel.savePoi(poi)
+
+        // When clear the livedata
+        saveReminderViewModel.onClear()
+
+        // Then viewModel's livedata is all null
+        assertThat(saveReminderViewModel.selectedPOI.getOrAwaitValue(), `is`(Matchers.nullValue()))
+        assertThat(saveReminderViewModel.reminderSelectedLocationStr.getOrAwaitValue(), `is`(Matchers.nullValue()))
+        assertThat(saveReminderViewModel.reminderTitle.getOrAwaitValue(), `is`(Matchers.nullValue()))
+        assertThat(saveReminderViewModel.latitude.getOrAwaitValue(), `is`(Matchers.nullValue()))
+    }
+
+    @Test
+    fun savePoi() {
+        // Given a Point of Interest
+        val poi =  PointOfInterest(
+            LatLng(-34.0,
+                151.0),
+            "0001",
+            "sydney"
+        )
+
+        // When save it to the viewModel properties
+        saveReminderViewModel.savePoi(poi)
+
+        // Then viewModel's livedata is updated
+        assertThat(saveReminderViewModel.selectedPOI.getOrAwaitValue(), `is`(poi))
+    }
 }
