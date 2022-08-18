11f98a99cc8a582a311d7f818d5d86e205bb51c0
Valery Basova <basovavalery@gmail.com>
2022-08-18 13:23:13 -0400

    add unit test for validateEnteredData method

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,12 +1,25 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+
+    private lateinit var viewModelTest: RemindersListViewModel
+    private lateinit var dataSource: FakeDataSource
+
+    @Before
+    fun setupViewModel() {
+        dataSource = FakeDataSource()
+        viewModelTest =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,78 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var dataSource: FakeDataSource
+    private lateinit var reminderDataItem1: ReminderDataItem
+    private lateinit var reminderDataItem2: ReminderDataItem
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    @Before
+    fun setupViewModel() {
+        dataSource = FakeDataSource()
+        saveReminderViewModel =
+            SaveReminderViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+        reminderDataItem1 = ReminderDataItem(
+            "",
+            "description",
+            "location",
+            -34.0,
+            151.0
+        )
+        reminderDataItem2 = ReminderDataItem(
+            "title",
+            "description",
+            "",
+            -34.0,
+            151.0
+        )
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun validateEnteredData_titleIsNullOrEmpty() {
+        // Given a reminderDataItem with empty title
+
+        // When validate title is null or Empty
+        val isTitleValid = saveReminderViewModel.validateEnteredData(reminderDataItem1)
+
+        // Then there is false returned
+        assertThat(isTitleValid, `is`(false))
+    }
+
+    @Test
+    fun validateEnteredData_locationIsNullOrEmpty() {
+        // Given a reminderDataItem with empty location
+
+        // When validate title is null or Empty
+        val isLocatonValid = saveReminderViewModel.validateEnteredData(reminderDataItem2)
+
+        // Then there is false returned
+        assertThat(isLocatonValid, `is`(false))
+    }
 
 }
